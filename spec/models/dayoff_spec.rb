3ae642708db2be4154834ef81bc82a46e65755dc
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-24 08:38:02 -0300

    10° commit - tdd model: day consegue ser preenchido? RED

---
+++ spec/models/dayoff_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Dayoff, type: :model do
+  
+  before{@dayoff = Dayoff.new}
+
+  describe 'testes de preenchimento do model Dayoff' do
+
+    it 'day consegue ser preenchido?' do
+      @dayoff.day = ''
+      expect(@dayoff.day).to eq('Natal')
+    end
+
+  end
+  
+end
